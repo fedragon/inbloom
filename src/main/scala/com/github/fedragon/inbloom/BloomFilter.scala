f14060df5daa2048d356901df2d5afb68b1e45de
fedragon <maffo7@gmail.com>
2013-09-06 20:02:55 +0200

    Generalized hashing function

--- src/main/scala/com/github/fedragon/inbloom/BloomFilter.scala
+++ src/main/scala/com/github/fedragon/inbloom/BloomFilter.scala
@@ -1,11 +1,31 @@
 package com.github.fedragon.inbloom
 
-case class BloomFilter(bits: Vector[Int] = (0 until 20).toVector) {
+case class BloomFilter(bits: Vector[Int] = (0 until 200).map(_ => 0).toVector) {
   
-  val functions: Seq[String => Int] = Seq(
-    (value: String) => value.hashCode % 5,
-    (value: String) => (value.hashCode % 5) + 5,
-    (value: String) => (value.hashCode % 5) + 10,
-    (value: String) => (value.hashCode % 5) + 15
-  )
+  def add(value: String): BloomFilter = {
+    val hashes = toHashes(value)
+
+    def update(updates: List[Int], result: Vector[Int]): Vector[Int] = {
+      updates match {
+        case Nil => result
+        case head :: tail => update(tail, result.updated(head, 1))
+      }
+    }
+
+    BloomFilter(update(hashes, bits))
+  }
+
+  def query(value: String) = {
+    toHashes(value).map(hash => bits(hash)).forall(b => b == 1)
+  }
+
+  private[inbloom] def toHashes(value: String): List[Int] = {
+    val ratio = 4
+    val modulo = bits.size / ratio
+    val hash = value.hashCode % modulo
+
+    (0 until ratio).map {
+      n => hash + modulo * n
+    }.toList
+  }
 }
--- src/test/scala/com/github/fedragon/inbloom/BloomFilterTest.scala
+++ src/test/scala/com/github/fedragon/inbloom/BloomFilterTest.scala
@@ -7,4 +7,30 @@ import org.scalatest.junit.JUnitRunner
 @RunWith(classOf[JUnitRunner])
 class BloomFilterTest extends FunSuite with BeforeAndAfter {
   
+  var initial: BloomFilter = _
+
+  before {
+    initial = new BloomFilter()
+  }
+
+  test("should initially contain only zeros") {
+    assert(initial.bits.forall(b => b == 0))
+  }
+
+  test("adding an element should return an updated BloomFilter") {
+    val updated = initial.add("Hello")
+
+    assert(initial.equals(updated) === false)
+  }
+
+  test("query an existing element should return true") {
+    val updated = initial.add("Hello")
+
+    assert(updated.query("Hello") === true)
+  }
+
+  test("query a non-existing element might return false") {
+    val hello = initial.add("Hello")
+    assert(hello.query("Dag") === false)
+  }
 }
